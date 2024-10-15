149db9fb3a5d7e22ebd244ded0c3d36b00b2da91
Nikola Rusakov <rusakov.nikola@gmail.com>
2024-10-15 15:03:22 +1000

    feat(bricks): add namespace for brick creation

--- components/polylith/bricks/brick.py
+++ components/polylith/bricks/brick.py
@@ -11,7 +11,7 @@ from polylith.readme import create_brick_readme
 def create_brick(root: Path, options: dict) -> None:
     modulename = options["modulename"]
     path_kwargs = {
-        k: v for k, v in options.items() if k in {"brick", "namespace_path", "package"}
+        k: v for k, v in options.items() if k in {"brick", "namespace_path", "namespace", "package"}
     }
 
     brick_structure = configuration.get_brick_structure_from_config(root)
